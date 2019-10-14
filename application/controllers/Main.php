cb557b02a4a47ca9220400b19728dfeb6be75eed
Alejandro <lopez.gutierrez.a07@gmail.com>
2019-10-14 19:55:52 -0400

    interfaz

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -3,6 +3,8 @@
 
 class Main extends CI_Controller {
 function index(){
+    $this->load->view('templete/header');
     $this->load->view('main');
+    $this->load->view('templete/footer');
 }
 }
---
+++ application/views/templete/footer.php
@@ -0,0 +1,26 @@
+<footer class="main-footer">
+        <div class="float-right d-none d-sm-block">
+            <b>Version</b> 1.0.0-b1
+        </div>
+        <strong>Copyright &copy; 2019<a href="#">Orden de Trabajo</a>.</strong> All rights
+        reserved.
+    </footer>
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="<?=base_url()?>plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?=base_url()?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?=base_url()?>dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?=base_url()?>dist/js/demo.js"></script>
+</body>
+</html>
